ae079208c0f96b71cf72c7fe0955b90e2a399f74
Collins77 <collinsmuemah@gmail.com>
2024-02-06 09:45:16 +0300

    updates

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
 // export const server = "https://eshop-tutorial.vercel.app/api/v2";
-export const server = "https://opasso-backend.vercel.app/api/v2";
+export const server = "https://reseller-sprint.vercel.app/api/v2";
 export const backend_url = "https://opasso-backend.vercel.app/"
 
 
